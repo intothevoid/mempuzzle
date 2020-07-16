fc3c5d5c5bb123ebbf38377e7ac8e78ca6cfba7f
Karan Kadam <karank@outlook.com>
2020-07-16 23:05:17 +0930

    add basic data structures

--- mempuzzle.py
+++ mempuzzle.py
@@ -1,4 +1,5 @@
 import sys
+import random
 import pygame
 from pygame import *
 
@@ -40,7 +41,8 @@ OVAL = 'oval'
 
 ALLCOLOURS = (RED, GREEN, BLUE, YELLOW, ORANGE, PURPLE, CYAN)
 ALLSHAPES = (DONUT, SQUARE, DIAMOND, LINES, OVAL)
-assert len(ALLCOLOURS) * len(ALLSHAPES) * 2 >= BOARDHEIGHT * BOARDWIDTH, "Board is too big for the number of shapes / colours"
+assert len(ALLCOLOURS) * len(ALLSHAPES) * 2 >= BOARDHEIGHT * \
+    BOARDWIDTH, "Board is too big for the number of shapes / colours"
 
 
 def main():
@@ -49,6 +51,9 @@ def main():
     FPSCLOCK = pygame.time.Clock()
     DISPLAYSURF = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
     pygame.display.set_caption("Memory Puzzle")
+
+    main_board = get_randomised_board()
+    revealed_boxes = get_revealed_boxes_data(False)
 
     DISPLAYSURF.fill(BGCOLOUR)
 
@@ -62,6 +67,38 @@ def main():
 
         pygame.display.update()
         FPSCLOCK.tick(FPS)
+
+
+def get_revealed_boxes_data(val):
+    revealed_boxes = []
+    for i in range(BOARDWIDTH):
+        revealed_boxes.append([val] * BOARDHEIGHT)
+    return revealed_boxes
+
+
+def get_randomised_board() -> list:
+    icons = []
+    for colour in ALLCOLOURS:
+        for shape in ALLSHAPES:
+            icons.append((colour, shape))
+
+    random.shuffle(icons)  # randomise the order of the icons
+
+    # no. of icons that fit on board
+    num_icons_used = int(BOARDHEIGHT * BOARDWIDTH / 2)
+    icons = icons[:num_icons_used] * 2  # make two of each
+    random.shuffle(icons)
+
+    # create the board data structure, with randomly placed icons
+    board = []
+    for x in range(BOARDWIDTH):
+        column = []
+        for y in range(BOARDHEIGHT):
+            column.append(icons.pop())
+
+        board.append(column)
+
+    return board
 
 
 if __name__ == "__main__":
